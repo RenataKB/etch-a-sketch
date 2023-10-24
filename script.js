b7e45cecf255ea8f7597515070a72191fe281728
Renata <renata.kb@gmail.com>
2023-10-24 20:55:37 -0300

    Show static 16x16 grid

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('div.container');
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        row.appendChild(square);          
+    }
+    container.appendChild(row);
+}
