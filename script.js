3824cbacd583c18bf4090210b69a15c0dbd53ef0
Renata <renata.kb@gmail.com>
2023-10-30 20:54:04 -0300

    Add grid size and reset buttons

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
 const container = document.querySelector('div.container');
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement('div');
-        square.setAttribute('class', 'square');
-        row.appendChild(square);          
+function createGrid(size=16) {
+    for (let i = 0; i < size; i++) {
+        const squareSize = 960/size;
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement('div');
+            square.setAttribute('class', 'square');
+            square.style.width = `${squareSize}px`;
+            square.style.height= `${squareSize}px`;
+            row.appendChild(square);          
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+    colorSquare();
 }
 
-const squares = document.querySelectorAll('div.square');
+function colorSquare() {
+    const squares = document.querySelectorAll('div.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        });
+    });
+}
 
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.setAttribute('style', 'background-color: black');
+function isValid(n) {
+    return Number.isInteger(n) && (n > 0 && n <= 100);
+}
+
+const sizeButton = document.querySelector('button.size');
+sizeButton.addEventListener('click', () => {
+    let size;
+    do {
+        size = prompt('Choose the size of the grid (max: 100):');
+        if (size === null) return;
+        size = parseInt(size, 10);
+    } while (!isValid(size));
+    container.textContent = '';
+    createGrid(size);
+});
+
+const resetButton = document.querySelector('button.reset');
+resetButton.addEventListener('click', () => {
+    const squares = document.querySelectorAll('div.square');
+    squares.forEach((square) => {
+        square.style.backgroundColor = 'white';
     });
 });
+
+createGrid();
